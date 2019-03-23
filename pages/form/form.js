861df8e9df4eebf1885590fffced055e8a7cfe33
谢飞飞 <xiefei741000@outlook.com>
2019-03-23 14:25:14 +0800

    用户反馈的内容保存到后端数据库

--- pages/form/form.js
+++ pages/form/form.js
@@ -1,16 +1,46 @@
 Page({
 
   data: {
-    array: ["中国", "美国", "俄国", "日本"],
-    index: 0,
-    data: "2019-04-03",
-    time: "12:33"
+    array: ["中国", "美国", "巴西", "日本"],
+    area: 0,
+    score: 0
   },
 
-  formSubmit(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  //地区选择
+  bindPickerChange: function(e) {
+    console.log('form发生了Picker事件，携带数据为：', e.detail.value)
+    this.setData({ area: e.detail.value} )
   },
-  formReset() {
-    console.log('form发生了reset事件')
+  //评分
+  bindSliderChange : function(e) {
+    console.log('form发生了Slider事件，携带数据为：', e.detail.value)
+    this.setData({ score: e.detail.value })
+  },
+
+  //表单提交
+  formSubmit(e) {
+    var formData = e.detail.value
+    formData.area = this.data.area
+    formData.score = this.data.score
+    console.log('form发生了submit事件，携带数据为：', formData)
+
+
+    var that = this
+
+    //发起网络请求
+    wx.request({
+      url: 'http://localhost:8080/weicms/index.php?s=/addon/Feedback/Feedback/addFeedback', // 仅为示例，并非真实的接口地址
+      data: formData,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+       
+      },
+      //显示加载中提示
+      complete: function () { 
+        
+      }
+    })
   }
 })
