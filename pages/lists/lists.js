1555c4fcfd922870196c6e7da3881b4c02d4f7c9
谢飞飞 <xiefei741000@outlook.com>
2019-03-23 09:53:40 +0800

    加了注释

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -19,6 +19,7 @@ Page({
     //在javascript中，this代表着当前对象，会随着程序的执行过程中的上下文改变
     ///把this对象复制到临时变量that
     var that = this
+
     //发起网络请求
     wx.request({
       url: 'http://localhost:8080/weicms/index.php?s=/addon/Cms/Cms/getlist', // 仅为示例，并非真实的接口地址
@@ -31,14 +32,15 @@ Page({
           {
             //提示没有更多数据了
             that.setData({ toastHidden: false })
+            //隐藏加载更多标签
             that.setData({ moreHidden: 'none' })
             return false
           }
-          console.log(res.data)
 
           var len = res.data.length
           that.setData( {lastid: res.data[len-1].id})
-
+          
+          //新旧内容拼接
           var dataArr = that.data.newsList
           var newData = dataArr.concat(res.data);
 
@@ -50,7 +52,8 @@ Page({
       complete:function() { that.setData({ loadHidden: true }) }
     })
   },
-
+  
+  //点击加载更多按钮的操作：提示一次网络状态 + 再次向服务器请求数据
   loadMore: function(event){
     //获取page中的data
     var id = event.currentTarget.dataset.lastid
@@ -63,9 +66,8 @@ Page({
         // 返回网络类型, 有效值：
         // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
         var networkType = res.networkType
-        if (networkType != "wifi" && isfirst==1){
-          that.setData({ confirmHidden:false})
-        }
+        //只对网络状态不在WIFI下提示一次
+        if (networkType != "wifi" && isfirst==1){ that.setData({ confirmHidden:false}) }
       }
     })
 
@@ -80,12 +82,13 @@ Page({
     
     this.loadData(0);
   },
-  
+
+  //提示没有更多数据
   totastChange: function(){
     this.setData({ toastHidden:true })
   },
+
+  //提醒网络状况
+  modalChange: function(){ this.setData({ confirmHidden: true }) }
   
-  modalChange: function(){
-    this.setData({ confirmHidden: true })
-  }
 })
