86ae3fde1672657b8a7cb9802f33cd053580f2bc
谢飞飞 <xiefei741000@outlook.com>
2019-03-24 15:40:27 +0800

    缓存数据方案优化

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -14,45 +14,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('detail.onLoad')
-    console.log(options)
-
-    var that = this
-    var key = 'info_' + options.id
-
-    //发起网络请求
-    wx.request({
-      url: 'http://localhost:8080/weicms/index.php?s=/addon/Cms/Cms/getDetail', // 仅为示例，并非真实的接口地址
-      data: { id: options.id },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success(res) {
-
-        console.log(res.data)
-
-        //利用setData设定数据
-        that.setData({ info: res.data })
-
-        //网络情况正常下，设置缓存
-        console.log(key)
-        wx.setStorageSync(key, res.data)
-        console.log('data from url')
-
-      },
-      //获取服务器数据失败
-      fail: function (res) {
-        //获取缓存
-        var info = wx.getStorageSync(key)
-        if (info) {
-          that.setData({ info: info })
-          console.log('data from cache')
-        }
-        else {
-          that.setData({ toastHidden: false, msg: '当前网络异常，请稍后再试' })
-        }
-      }
-    })
+    //调用接口
+    var common = require('../../utils/common.js')
+    common.loadInfo(options.id,this)
   },
 
   //返回上一页
---
+++ utils/common.js
@@ -0,0 +1,42 @@
+//先从缓存中获取数据，若无，则从server获取
+function loadInfo(id,obj){
+  var key = 'info_' + id
+  //获取缓存
+  var info = wx.getStorageSync(key)
+  if(info){
+    obj.setData({ info: info })
+    console.log('data from localCache')
+    return true
+  }
+
+  //发起网络请求
+  wx.request({
+    url: 'http://localhost:8080/weicms/index.php?s=/addon/Cms/Cms/getDetail', // 仅为示例，并非真实的接口地址
+    data: { id: id },
+    header: {
+      'content-type': 'application/json' // 默认值
+    },
+    success(res) {
+
+      console.log(res.data)
+
+      //利用setData设定数据
+      obj.setData({ info: res.data })
+
+      //网络情况正常下，设置缓存
+      console.log(key)
+      wx.setStorageSync(key, res.data)
+      console.log('data from server')
+
+    },
+    //获取服务器数据失败
+    fail: function (res) {
+      console.log('server error')
+      obj.setData({ toastHidden: false, msg: '当前网络异常，请稍后再试' })
+    }
+  })
+}
+//暴露接口
+module.exports = {
+  loadInfo: loadInfo
+}
