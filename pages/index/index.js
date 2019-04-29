2d3da8242db4ca60d0f1e80bee00888fcf0ed511
xiefeifei <xiefei741000@outlook.com>
2019-04-29 10:11:07 +0800

    调整tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,28 +15,11 @@ Page({
     }, {
       icon: '/images/tsg.png',
       src: '/pages/core/exam/exam',
-      title: "图书馆"
+      title: "成绩"
     }, {
       icon: '/images/kc.png',
       src: '/pages/core/library/library',
-      title: "考场"
-    }, {
-      icon: '/images/cj.png',
-      src: '/pages/core/campusCard/campusCard',
-      title: '成绩'
-    }, {
-      icon: '/images/ykt.png',
-      src: '/pages/core/net/net',
-      title: '一卡通'
-    }, {
-      icon: '/images/xxfk.png',
-      src: '/pages/core/electric/electric',
-      bindtap: 'KFZ',
-      title: "信息反馈"
-    }, {
-      icon: '/images/info.png',
-      src: '/pages/core/finance/finance',
-      title: '校园信息'
+      title: "考试信息"
     }]
   },
 
--- pages/rooms/rooms.js
+++ pages/rooms/rooms.js
@@ -132,7 +132,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    // console.log("转到身份认证界面")
+    // wx.switchTab({
+    //   url: '../account/account',
+    // })
   },
 
   /**
