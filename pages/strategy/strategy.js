eaeecc8cf18f4389f0c6a2dc1d165c918f12ca24
xiefeifei <xiefei741000@outlook.com>
2019-05-07 10:37:08 +0800

    实现动态渲染标签页

--- pages/strategy/strategy.js
+++ pages/strategy/strategy.js
@@ -1,7 +1,29 @@
 var app = getApp()
 Page({
+
+  data:{
+    organizationArray: [],  //组织机构
+    ilovelearnArray:[]   //我爱学习
+  },
+
   onLoad: function (options) {
     console.log('onLoad: 加载strategy页面')
+
+    var that = this
+    var lastid = 0
+
+    //动态渲染标签列表
+    wx.request({
+      url: 'https://www.bjutxiaomei.cn/index.php?s=/addon/Organization/Organization/getOrganization', // 真实接口地址
+      data: { lastid: lastid },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({ organizationArray: res.data })
+      }
+    })
  },
 
   //查找不同类型文章
