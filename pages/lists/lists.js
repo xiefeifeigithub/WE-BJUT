568ac36c4465ee038b86d3c244c3840a6c437d73
Berlin1122 <c18811226033@163.com>
2019-05-10 13:48:02 +0800

    优化文章加载

--- pages/lists/lists.js
+++ pages/lists/lists.js
@@ -11,7 +11,7 @@ Page({
     msg: '没有更多文章了',
   },
 
-  loadData:function (lastid){
+  loadData: function (lastid) {
     console.log('向服务器请求的初始元组id: ' + lastid)
 
     var limit = 10 //设置一次性文章加载数量
@@ -23,45 +23,50 @@ Page({
     //发起网络请求
     wx.request({
       url: 'https://www.bjutxiaomei.cn/index.php?s=/addon/Cms/Cms/getlist', // 真实接口地址
-      data: {lastid:lastid, limit:limit,type:type},
+      data: { lastid: lastid, limit: limit, type: type },
       header: {
         'content-type': 'application/json' // 默认值
       },
       success(res) {
         console.log(res.data)
-          if(!res.data)
-          {
-            //提示没有更多数据了
-            that.setData({ toastHidden: false })
-            //隐藏加载更多按钮
-            that.setData({ moreHidden: 'none' })
-            return false
-          }
+        if (!res.data) {
+          wx.hideLoading()
+          //提示没有更多数据了
+          that.setData({ toastHidden: false })
+          //隐藏加载更多按钮
+          that.setData({ moreHidden: 'none' })
+          wx.showToast({
+            title: '没有更多了...',
+            icon:'none'
+          })
+          return false
+        }else{
           //更新lastid
-        console.log(lastid)
+          console.log(lastid)
           var len = res.data.length
           var oldLastid = lastid
-          that.setData( {lastid: res.data[len-1].id})
-        console.log(lastid)
-          
+          that.setData({ lastid: res.data[len - 1].id })
+          console.log(lastid)
+
           //新旧内容拼接
           var dataArr = that.data.newsList
           var newData = dataArr.concat(res.data);
 
           //设置文章数据缓存
-          if(oldLastid==0){
-             wx.setStorageSync('CmsList',newData)
+          if (oldLastid == 0) {
+            wx.setStorageSync('CmsList', newData)
           }
 
           //利用setData设定数据
           that.setData({ newsList: newData })
           that.setData({ moreHidden: '' })
-          
-          console.log('data from url')
+        
+        }
+       
       },
 
       //获取服务器数据失败
-      fail: function(res){
+      fail: function (res) {
         if (lastid == 0) {
           //获取缓存
           var newData = wx.getStorageSync('CmsList')
@@ -81,53 +86,35 @@ Page({
       }
     })
   },
-  
-  //点击加载更多按钮的操作：提示一次网络状态 + 再次向服务器请求数据
-  loadMore: function(event){
-    //获取page中的data
-    var id = event.currentTarget.dataset.lastid
-    var isfirst = event.currentTarget.dataset.isfirst
- 
-    var that = this
-
-    wx.getNetworkType({
-      success: function (res) {
-        // 返回网络类型, 有效值：
-        // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
-        var networkType = res.networkType
-        //只对网络状态不在WIFI下提示一次
-        if (networkType != "wifi" && isfirst==1){ that.setData({ confirmHidden:false}) }
-      }
-    })
-
-    this.setData({ isfirst:0})
-    this.loadData(id);
-  },
 
   //监听页面加载 页面加载时触发
   onLoad: function (options) {
     console.log('onLoad: 加载lists页面')
     console.log(options)
     var that = this
-    
+
     //请求数据
     console.log("开始向向服务器请求文章列表数据，从id=0开始请求")
     this.loadData(0);
   },
-   
+
   //页面初次渲染完成时触发
-  onReady: function(){
+  onReady: function () {
     //动态设置当前页面的标题
     wx.setNavigationBarTitle({
       title: '文章列表'
     })
   },
 
-  //提示没有更多数据
-  totastChange: function(){
-    this.setData({ toastHidden:true })
+  touchArticle:function(options){
+    console.log(options.currentTarget.dataset.id)
+    wx.navigateTo({
+      url: '../../pages/detail/detail?id='+options.currentTarget.dataset.id,
+    })
   },
-
-  //提醒网络状况
-  modalChange: function(){ this.setData({ confirmHidden: true }) }
+  tolower:function(){
+    console.log("触发加载操作")
+    this.loadData(this.data.lastid)
+  },
+  
 })
