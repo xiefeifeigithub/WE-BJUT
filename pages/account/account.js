58a1d5fd2a2fbd5a0b5a3a97d69580e7509a204c
liyifeng <787339520@qq.com>
2019-05-11 23:06:15 +0800

    取消滑动

--- pages/account/account.js
+++ pages/account/account.js
@@ -34,18 +34,7 @@ Page({
       this.setData({ unload: true })
     }
   },
-  onShow: function () {
-    app.globalData.flag_hd = true;    //重新进入页面之后，可以再次执行滑动切换页面代码
-    clearInterval(app.globalData.interval); // 清除setInterval
-    app.globalData.time = 0;
 
-  },
-  touchStart: function (e) {
-    common.touchStart(e)
-  },
-  touchEnd: function (e) {
-    common.touchEndaccount(e)
-  },
 
   // 生命周期函数--监听页面初次渲染完成
   onReady: function () {
