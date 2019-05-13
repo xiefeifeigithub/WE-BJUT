5c16781b1f6627fc0a547078767e81db96235c8d
xiefeifei <xiefei741000@outlook.com>
2019-05-13 16:20:40 +0800

    解决老用户版本更新问题

--- app.js
+++ app.js
@@ -31,7 +31,41 @@ App({
     flag_hd : true
   },
 
+  updataVersion: function () {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+      wx.showModal({
+        title: '已经有新版本了哟~',
+        content: '新版本已经上线啦~，请您删除当前小程序，重新搜索工大小美后打开呦 ',
+      })
+    })
+  },
+
   onLaunch: function () {
+    
+    //尝试获取新版本
+    this.updataVersion()
+
     //从缓存中获取用户信息
     console.log("从缓存中获取用户信息")
     var username = wx.getStorageSync(this.data.keyUserName)
